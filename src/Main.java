8cff7e3bd18a88a42899880e60454364c9532519
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-11-05 08:42:51 -0500

    d

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        String ssn = InputHelper.getRegExString(scan,"Enter your SSN","\\d{3}-\\d{2}-\\d{4}");
+        System.out.println("Your SSN is: " + ssn);
+        String uCID = InputHelper.getRegExString(scan, "Enter your Student M Number:", "(M|m)\\d{5}");
+        System.out.println("Your student M Number is: " + uCID);
+        String menuChoice = InputHelper.getRegExString(scan, "Menu:   O - Open   S - Save   V - View   Q - Quit", "[OoSsVvQq]");
+        System.out.println("You choose: " + menuChoice);
     }
 }
